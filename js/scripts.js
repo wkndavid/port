ab5397c49199f313d0f3515c97d839d4fccf9da1
wkndavid <workingnown@gmail.com>
2021-09-07 22:17:01 -0300

    Commit Update-Code

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,26 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+    
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
